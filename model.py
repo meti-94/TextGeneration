30e83417504805576923418e13e3c12ac3c75b9e
meti-94 <mehdi_j94@outlook.com>
2021-07-01 17:01:46 +0430

    adding closs

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 """
 Training loop 
 """
-from transformers import BertModel, Seq2SeqTrainingArguments, AutoModel, Seq2SeqTrainer, BertConfig, EncoderDecoderModel, AutoModelForCausalLM, EncoderDecoderConfig
+from transformers import BertModel, Seq2SeqTrainingArguments, AutoModel, BertConfig, EncoderDecoderModel, AutoModelForCausalLM, EncoderDecoderConfig
 
 from transformers.modeling_outputs import Seq2SeqLMOutput
 
@@ -11,9 +11,17 @@ from collections import namedtuple
 
 import datasets
 
+from transformers import Seq2SeqTrainer
+
+import sys
+
+from torch.nn import CosineSimilarity
+
 import logging
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
+
+from transformers import logging as tlogging
 
 
 def model_init(model):
@@ -49,6 +57,7 @@ class SimpleEncoderDecoder(torch.nn.Module):
 		self.encoder.resize_token_embeddings(len(self.tokenizer))
 		self.decoder.resize_token_embeddings(len(self.tokenizer))
 
+		self.cs = CosineSimilarity()
 	def forward(
 		self,
 		input_ids=None,
@@ -67,7 +76,7 @@ class SimpleEncoderDecoder(torch.nn.Module):
 		**kwargs,
 		):
 		
-
+		custom_loss = None
 		if encoder_outputs is None:
 			encoder_outputs = self.encoder(
 				input_ids=input_ids,
@@ -77,6 +86,39 @@ class SimpleEncoderDecoder(torch.nn.Module):
 				output_hidden_states=output_hidden_states,
 				return_dict=return_dict,
 			)
+			# first speaker 1 occurrence
+			persona_boundaries = (input_ids == 30525).nonzero(as_tuple=False)
+			boundaries = []
+			for item in persona_boundaries.tolist():
+				if item[0] not in [x[0] for x in boundaries]:
+					boundaries.append(item)
+			boundaries = [item[1] for item in boundaries]
+			persona_input_ids, persona_attention_mask = [], []
+			for i, (ids, attention) in enumerate(zip(input_ids, attention_mask)):
+				persona_input_ids.append(ids.tolist()[:boundaries[i]+1]+[30524 for _ in range(512-(boundaries[i]+1))])
+				persona_attention_mask.append(attention.tolist()[:boundaries[i]+1]+[0 for _ in range(512-(boundaries[i]+1))]) 
+			device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+			persona_input_ids = torch.Tensor(persona_input_ids).long().to(device)
+			persona_attention_mask = torch.Tensor(persona_attention_mask).long().to(device)
+			custom_encoder_outputs = self.encoder(
+				input_ids=persona_input_ids,
+				attention_mask=persona_attention_mask,
+				inputs_embeds=inputs_embeds,
+				output_attentions=output_attentions,
+				output_hidden_states=output_hidden_states,
+				return_dict=return_dict,
+			)
+			custom_loss = self.cs(
+				encoder_outputs['pooler_output'],
+				custom_encoder_outputs['pooler_output'],
+				).mean()
+			# print(custom_encoder_outputs.keys(), type(custom_encoder_outputs['pooler_output']), custom_encoder_outputs['pooler_output'].size())
+			# print(custom_loss)
+			# sys.exit()
+			
+			# after 1st occurrence convert to 0 
+			# attention, input is 
+			# custom loss  
 
 		encoder_hidden_states = encoder_outputs[0]
 
@@ -101,7 +143,8 @@ class SimpleEncoderDecoder(torch.nn.Module):
 		return Seq2SeqLMOutput(
 			loss=decoder_outputs.loss,
 			logits=decoder_outputs.logits,
-			# past_key_values=decoder_outputs.past_key_values,
+			# closs = custom_loss, 
+			past_key_values=custom_loss,
 			decoder_hidden_states=decoder_outputs.hidden_states,
 			decoder_attentions=decoder_outputs.attentions,
 			# cross_attentions=decoder_outputs.cross_attentions,
@@ -126,8 +169,30 @@ class SimpleEncoderDecoder(torch.nn.Module):
 		return huggingface_model 
 
 	
-	# def model_init():
-	#     return model
+	def generate(self, dataset):
+		huggingface_model = self.convert_to_huggingface()
+		huggingface_model.config.decoder_start_token_id = self.tokenizer.cls_token_id
+		huggingface_model.config.eos_token_id = self.tokenizer.sep_token_id
+		huggingface_model.config.pad_token_id = self.tokenizer.pad_token_id
+		huggingface_model.config.vocab_size = huggingface_model.config.encoder.vocab_size
+		huggingface_model.config.add_cross_attention=True
+		huggingface_model.config.no_repeat_ngram_size = 3
+		huggingface_model.config.early_stopping = True
+		huggingface_model.config.length_penalty = 2.0
+		huggingface_model.config.num_beams = 4
+		util_args = Seq2SeqTrainingArguments(
+			predict_with_generate=True,
+			output_dir='./tmp'
+			)
+		util = Seq2SeqTrainer(
+			args=util_args,
+			model=huggingface_model,
+    		compute_metrics=lambda pred:compute_metrics(pred, self.tokenizer),
+    		eval_dataset=dataset,
+			tokenizer=self.tokenizer,
+			)
+		return util.predict(dataset)
+
 
 	def automatic_metrics(self, dataset):
 		huggingface_model = self.convert_to_huggingface()
@@ -151,50 +216,12 @@ class SimpleEncoderDecoder(torch.nn.Module):
     		eval_dataset=dataset,
 			tokenizer=self.tokenizer,
 			)
+		# tlogging.set_verbosity_info()
 		return util.evaluate()
 
 
 
 	
-	def forward_old(
-			self,
-			input_ids=None,
-			attention_mask=None,
-			decoder_input_ids=None,
-			decoder_attention_mask=None,
-			encoder_outputs=None,
-			labels=None,
-			return_dict=True
-		):
-		# Encode
-		if encoder_outputs is None:
-			encoder_outputs = self.encoder(
-					input_ids=input_ids,
-					attention_mask=attention_mask,
-					return_dict=return_dict
-				)
-
-		encoder_hidden_states = encoder_outputs[0]
-		# Decode
-		decoder_outputs = self.decoder(
-				input_ids=decoder_input_ids,
-				attention_mask=decoder_attention_mask,
-				encoder_hidden_states=encoder_hidden_states,
-				labels=labels,
-				return_dict=return_dict
-			)
-		if not return_dict:
-			return decoder_outputs.loss, decoder_outputs.logits
-		
-		return Seq2SeqLMOutput(
-			loss=decoder_outputs.loss,
-			logits=decoder_outputs.logits,
-			decoder_hidden_states=decoder_outputs.hidden_states,
-			decoder_attentions=decoder_outputs.attentions,
-			encoder_last_hidden_state=encoder_outputs,
-		)
-	
-
 
 
 def greedy_generate(
@@ -224,6 +251,6 @@ def save(model, save_path='./models/bert_encoder_decoder.pt'):
 		logging.info(f'Model is saved in {save_path}')
 
 def load(save_path='./models/bert_encoder_decoder.pt'):
-	model = torch.load(save_path, map_location=torch.device('cpu'))
+	model = torch.load(save_path)
 	logging.info(f'Model is loaded from {save_path}')
 	return model 
--- train.py
+++ train.py
@@ -25,6 +25,9 @@ import logging
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
+from transformers import logging as tlogging
+# tlogging.set_verbosity_error()
+
 ####################################################
 ############## Preparing Tokenizer and Model #######
 ####################################################
@@ -42,7 +45,7 @@ model = SimpleEncoderDecoder(tokenizer)
 ######### Preparing Train, Test & Validation #######
 ####################################################
 
-train = PersonaChatDataset_v3('./data/personachat_self_original.json', tokenizer, test=False)
+train = PersonaChatDataset_v3('./data/personachat_self_original.json', tokenizer, test=True)
 test = PersonaChatDataset_v3('./data/personachat_self_original.json', tokenizer, test=True)
 # train_dataset = PersonaChatPytorchDataset_v3(train)
 train, valid = train_test_split(train.model_food, test_size=0.1, random_state=44)
@@ -51,8 +54,8 @@ trainset = train
 validset = valid
 testset = test.model_food[:10]
 
-train_dataloader = DataLoader(PersonaChatPytorchDataset_v3(trainset), batch_size=64, shuffle=True)
-valid_dataloader = DataLoader(PersonaChatPytorchDataset_v3(validset), batch_size=64, shuffle=False)
+train_dataloader = DataLoader(PersonaChatPytorchDataset_v3(trainset), batch_size=4, shuffle=True)
+valid_dataloader = DataLoader(PersonaChatPytorchDataset_v3(validset), batch_size=2, shuffle=False)
 test_dataloader = DataLoader(PersonaChatPytorchDataset_v3(testset), batch_size=1, shuffle=False)
 
 ####################################################
@@ -79,42 +82,39 @@ model.to(device)
 step = 0
 for epoch in range(epochs):
 	
-	losses = []
-
+	train_losses = []
+	train_custom_losses = []
 	for _, batch in enumerate(tqdm(train_dataloader, desc=f"Train Epoch Number {epoch+1}")):
 		model.train()
 		model.zero_grad()
 		device_batch = {key:value.to(device) for key, value in batch.items()}
-		loss = model(**device_batch, return_dict=True)['loss']
-
-		losses.append(loss)
+		model_output = model(**device_batch, return_dict=True)
+		loss = model_output['loss']
+		closs = model_output['past_key_values']
+		train_losses.append(loss)
+		train_custom_losses.append(closs)
 		loss.backward()
 		optimizer.step()
 		step+=1
-		if step%200==0:
+		if step%10==0:
 			model.eval()
 			losses = []
+			custom_losses = []
 			for _, batch in enumerate(tqdm(valid_dataloader, desc=f"Eval Epoch Number {epoch+1}")):
 				with torch.no_grad():
 					device_batch = {key:value.to(device) for key, value in batch.items()}
-					loss = model(**device_batch, return_dict=True)['loss']
+					model_output = model(**device_batch, return_dict=True)
+					loss = model_output['loss']
+					closs = model_output['past_key_values']
 					losses.append(loss)
+					custom_losses.append(closs)
 			logging.info(f'Epoch number: {epoch+1} Eval Loss is equal: {sum(losses)/len(losses)}')
-
-			for _, batch in enumerate(test_dataloader):
-				texts = []
-				device_batch = {key:value.to(device) for key, value in batch.items()}
-				greedy_generate(
-						input_ids=device_batch['input_ids'],
-						device=device,
-						model=model,
-						tokenizer=tokenizer,
-					   )
-
-			logging.info(model.automatic_metrics(validset)
+			logging.info(f'Epoch number: {epoch+1} Eval cLoss is equal: {sum(custom_losses)/len(custom_losses)}')
+			logging.info(model.automatic_metrics(validset))
 
 		if step%1000==0:
 			save(model)
-	logging.info(f'Epoch number: {epoch+1} Train Loss is equal: {sum(losses)/len(losses)}') 
+	logging.info(f'Epoch number: {epoch+1} Train Loss is equal: {sum(train_losses)/len(train_losses)}') 
+	logging.info(f'Epoch number: {epoch+1} Train Loss is equal: {sum(train_custom_losses)/len(train_custom_losses)}') 
 	
 	
